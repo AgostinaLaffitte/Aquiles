f3f4dba595776c9de20e53846c279245c827ab08
Agostina <agoslaffitte17@gmail.com>
2026-06-03 00:36:58 -0300

    modificando archivo cloudinary

--- backend-aquiles/src/cloudinary/cloudinary.service.ts
+++ backend-aquiles/src/cloudinary/cloudinary.service.ts
@@ -3,26 +3,28 @@ import { v2 as cloudinary } from 'cloudinary';
 import { UploadApiResponse } from 'cloudinary';
 import * as toStream from 'buffer-to-stream'; // Asegúrate de instalarlo
 import { ConfigService } from '@nestjs/config/dist/config.service';
+import { Readable } from 'stream';
 
 @Injectable()
 export class CloudinaryService {
 
-  
-  
-
- async uploadImage(file: Express.Multer.File): Promise<UploadApiResponse> {
+async uploadImage(file: Express.Multer.File): Promise<UploadApiResponse> {
     return new Promise((resolve, reject) => {
-      // Verificación de seguridad
-      if (!process.env.CLOUDINARY_NAME) {
-        return reject(new Error("Cloudinary no está configurado (variables faltantes)"));
-      }
+      // Creamos un stream a partir del buffer manualmente
+      const readableStream = new Readable({
+        read() {
+          this.push(file.buffer);
+          this.push(null);
+        }
+      });
 
       const upload = cloudinary.uploader.upload_stream((error, result) => {
         if (error) return reject(error);
         if (!result) return reject(new Error("Error al subir imagen"));
         resolve(result);
       });
-      toStream(file.buffer).pipe(upload);
+
+      readableStream.pipe(upload);
     });
   }
 }
