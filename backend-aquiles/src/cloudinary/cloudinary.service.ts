6fe2945dd35b640e29d4c755f2059108b69cfa0f
Agostina <agoslaffitte17@gmail.com>
2026-06-02 23:52:00 -0300

    solucionando error cloudinary

--- backend-aquiles/src/cloudinary/cloudinary.service.ts
+++ backend-aquiles/src/cloudinary/cloudinary.service.ts
@@ -5,13 +5,7 @@ import * as toStream from 'buffer-to-stream'; // Asegúrate de instalarlo
 
 @Injectable()
 export class CloudinaryService {
-  constructor() {
-    cloudinary.config({
-      cloud_name: process.env.CLOUDINARY_NAME,
-      api_key: process.env.CLOUDINARY_API_KEY,
-      api_secret: process.env.CLOUDINARY_API_SECRET,
-    });
-  }
+  
 
   async uploadImage(file: Express.Multer.File): Promise<UploadApiResponse> {
     return new Promise((resolve, reject) => {
